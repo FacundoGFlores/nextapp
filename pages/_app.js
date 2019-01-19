e769d0fb218e064e4a73cc10301e1fd7ababde68
Facundo Flores <flores.facundogabriel@gmail.com>
2019-01-19 13:32:48 +0100

    Add mui setup from repo

---
+++ pages/_app.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import App, { Container } from "next/app";
+import Head from "next/head";
+
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
+import JssProvider from "react-jss/lib/JssProvider";
+
+import getPageContext from "../src/utils/getPageContext";
+
+export default class NextApp extends App {
+  constructor() {
+    super();
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Head>
+          <title>Next App</title>
+        </Head>
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+                tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* Pass pageContext to the _document though the renderPage enhancer
+                  to render collected styles on server-side. */}
+            <Component pageContext={this.pageContext} {...pageProps} />
+          </MuiThemeProvider>
+        </JssProvider>
+      </Container>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-export default () => <div>This is a page rendered with nextjs.</div>;
+import Button from "@material-ui/core/Button";
+
+export default () => (
+  <React.Fragment>
+    <Button variant="contained" color="primary">
+      primary button
+    </Button>
+    <Button variant="contained" color="secondary">
+      secondary button
+    </Button>
+  </React.Fragment>
+);
