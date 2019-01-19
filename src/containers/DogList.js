25b5833ae053b5fdc400346d4d1794cf34c0df13
Facundo Flores <flores.facundogabriel@gmail.com>
2019-01-19 16:06:44 +0100

    Support lazy loading

--- src/containers/DogList.js
+++ src/containers/DogList.js
@@ -1,24 +1,61 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import LazyLoad from "react-lazyload";
 
 import Dog from "../components/Dog";
 
-const DogList = ({ dogs }) => (
-  <React.Fragment>
-    {dogs.length ? (
-      dogs.map(dog => (
-        <Grid item xs key={dog}>
-          <Dog url={dog} title={dog.split("/")[4]} />
-        </Grid>
-      ))
-    ) : (
-      <Typography>No dogs</Typography>
-    )}
-  </React.Fragment>
-);
+class DogList extends React.Component {
+  state = {
+    dogs: this.props.dogs,
+    lazyDogs: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://dog.ceo/api/breeds/image/random/50")
+      .then(response => response.data)
+      .then(({ message }) => {
+        this.setState({ lazyDogs: message });
+      });
+  }
+  render() {
+    const { dogs } = this.props;
+    const { lazyDogs } = this.state;
+
+    return (
+      <React.Fragment>
+        {dogs.length ? (
+          dogs.map(dog => (
+            <Grid item xs key={dog}>
+              <Dog url={dog} title={dog.split("/")[4]} />
+            </Grid>
+          ))
+        ) : (
+          <Typography>No dogs</Typography>
+        )}
+        {lazyDogs.map(dog => (
+          <LazyLoad
+            placeholder={<CircularProgress size={80} />}
+            key={dog}
+            once
+            debounce={500}
+            height={200}
+          >
+            <Grid item xs>
+              <Dog url={dog} title={dog.split("/")[4]} />
+            </Grid>
+          </LazyLoad>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
 
 DogList.defaultProps = {
   dogs: []
